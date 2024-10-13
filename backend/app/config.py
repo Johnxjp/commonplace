7f50ab9ae28ed60eff7823bc7eb0030c96eff69a
John Lingi <16599457+Johnxjp@users.noreply.github.com>
2024-10-13 18:11:31 +0100

    Add config file

---
+++ backend/app/config.py
@@ -0,0 +1,22 @@
+import os
+import logging
+
+# TODO: Move all these to env files
+DB_DRIVER = "postgresql"
+DB_USERNAME = "postgres"
+DB_NAME = "conex"
+DB_HOST = "localhost"
+DB_PORT = 5432
+
+EMBEDDING_DIMENSIONS = 512
+TFHUB_CACHE_DIR = os.path.join("dump/tfhub_models")
+CHUNKING_STRATEGY = None
+THRESHOLD_SCORE = 1.0
+
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
