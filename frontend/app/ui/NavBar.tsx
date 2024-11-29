b485432d30b608408d5b8a18ae55a47412bbcada
John Lingi <16599457+Johnxjp@users.noreply.github.com>
2024-11-29 21:48:22 +0000

    Fixed navbar buttons

--- frontend/app/ui/NavBar.tsx
+++ frontend/app/ui/NavBar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NavBar({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
@@ -6,23 +8,38 @@ export default function NavBar({
 			<div className="flex-1 w-16 max-w-16 h-full bg-orange-100 px-3">
 				<div className="flex flex-col justify-center gap-y-3 pt-10">
 					<button
-						className="h-8 w-8 font-medium bg-white flex items-center justify-center rounded-md hover:shadow-sm hover:border-md"
+						className="h-8 w-8 font-medium bg-white rounded-md hover:shadow-sm hover:border-md"
 						title="home"
 					>
-						H
+						<Link
+							className="flex items-center justify-center h-full w-full"
+							href="/"
+						>
+							H
+						</Link>
 					</button>
 					<button
-						className="h-8 w-8 font-medium bg-white flex items-center justify-center rounded-md hover:shadow-sm hover:border-md"
+						className="h-8 w-8 font-medium bg-white rounded-md hover:shadow-sm hover:border-md"
 						title="library"
 					>
-						L
+						<Link
+							className="flex items-center justify-center h-full w-full"
+							href="/library"
+						>
+							L
+						</Link>
 					</button>
 
 					<button
-						className="h-8 w-8 font-medium bg-white flex items-center justify-center rounded-md hover:shadow-sm hover:border-md"
+						className="h-8 w-8 font-medium bg-white rounded-md hover:shadow-sm hover:border-md"
 						title="search_history"
 					>
-						S
+						<Link
+							className="flex items-center justify-center h-full w-full"
+							href="/search"
+						>
+							S
+						</Link>
 					</button>
 				</div>
 			</div>
