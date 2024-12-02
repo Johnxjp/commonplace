dd99c1384d18449cc2f86fe9a38a564fb28b015e
John Lingi <16599457+Johnxjp@users.noreply.github.com>
2024-12-02 19:31:15 +0000

    Load document page when click on document

---
+++ frontend/app/document/[id]/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import BookDocument from "@/definitions";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Document() {
+	const [document, setDocument] = useState<BookDocument | null>(null);
+	const params = useParams();
+	console.log(params);
+
+	// use effect to fetch document data
+	useEffect(() => {
+		const serverUrl = "http://localhost:8000";
+		const resourceUrl = serverUrl + "/documents/" + params.id;
+		const requestParams = {
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		};
+
+		fetch(resourceUrl, requestParams)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				const document: BookDocument = {
+					id: data.id,
+					title: data.title,
+					authors: [data.authors],
+					documentType: data.document_type,
+					content: data.content,
+					createdAt: new Date(data.created_at),
+					updatedAt: data.updated_at ? new Date(data.updated_at) : null,
+					isClip: data.is_clip,
+					clipStart: data.clip_start,
+					clipEnd: data.clip_end,
+					catalogueId: data.catalogue_id,
+				};
+				console.log(document);
+				setDocument(document);
+			})
+			.catch((err) => console.error(err));
+	}, []);
+
+	return (document === null ? <></> : 
+            <div className="flex flex-col items-center">
+                <h1>{document.title}</h1>
+                <h2>{document.authors.join(", ")}</h2>
+                <p>{document.content}</p>
+            </div>
+	);
+}
--- frontend/app/ui/BookDocumentCard.tsx
+++ frontend/app/ui/BookDocumentCard.tsx
@@ -1,9 +1,17 @@
 import BookDocument from "@/definitions";
 import { capitalizeFirstLetter } from "@/utils";
+import { useRouter } from "next/navigation";
 
 type BookDocumentCardProps = BookDocument;
 
 export default function BookDocumentCard(document: BookDocumentCardProps) {
+	const router = useRouter();
+
+	function handleOnClick() {
+		// open the document
+		router.push(`/document/${document.id}`);
+	}
+
 	function formatContent(content: string): string {
 		const charsToTrim = `"“'`;
 		content = content.trim();
@@ -19,7 +27,7 @@ export default function BookDocumentCard(document: BookDocumentCardProps) {
 	}
 
 	return (
-		<div className="h-full w-full border border-border-300 hover:border-border-200 group relative flex cursor-pointer flex-col gap-1.5 rounded-xl py-5 px-6 transition-all ease-in-out hover:shadow-sm active:scale-[0.98] md:gap-4">
+		<div onClick={() => handleOnClick()} className="h-full w-full border border-border-300 hover:border-border-200 group relative flex cursor-pointer flex-col gap-1.5 rounded-xl py-5 px-6 transition-all ease-in-out hover:shadow-sm active:scale-[0.98] md:gap-4">
 			<h2 className=" text-sm line-clamp-1">{document.title}</h2>
 			<p className="text-sm italic line-clamp-3">
 				{formatContent(document.content)}
