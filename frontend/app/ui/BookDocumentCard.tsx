965980c0760498659baf474cd35b0a771af53b20
John Lingi <16599457+Johnxjp@users.noreply.github.com>
2024-12-02 19:37:05 +0000

    Load annotation page when click on annotation

--- frontend/app/ui/BookDocumentCard.tsx
+++ frontend/app/ui/BookDocumentCard.tsx
@@ -9,7 +9,7 @@ export default function BookDocumentCard(document: BookDocumentCardProps) {
 
 	function handleOnClick() {
 		// open the document
-		router.push(`/document/${document.id}`);
+		router.push(`/annotation/${document.id}`);
 	}
 
 	function formatContent(content: string): string {
@@ -27,7 +27,10 @@ export default function BookDocumentCard(document: BookDocumentCardProps) {
 	}
 
 	return (
-		<div onClick={() => handleOnClick()} className="h-full w-full border border-border-300 hover:border-border-200 group relative flex cursor-pointer flex-col gap-1.5 rounded-xl py-5 px-6 transition-all ease-in-out hover:shadow-sm active:scale-[0.98] md:gap-4">
+		<div
+			onClick={() => handleOnClick()}
+			className="h-full w-full border border-border-300 hover:border-border-200 group relative flex cursor-pointer flex-col gap-1.5 rounded-xl py-5 px-6 transition-all ease-in-out hover:shadow-sm active:scale-[0.98] md:gap-4"
+		>
 			<h2 className=" text-sm line-clamp-1">{document.title}</h2>
 			<p className="text-sm italic line-clamp-3">
 				{formatContent(document.content)}
